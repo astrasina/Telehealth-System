c7579106f61a1952f0aa3e675696b4c5cec570ee
astrasina <rm.milar00043@student.tsu.edu.ph>
2025-12-28 00:45:41 +0800

    fixed, added, removed, bleep

--- mockup.js
+++ mockup.js
@@ -6,15 +6,12 @@ function toggleForm() {
     if (loginRadio.checked) {
         loginForm.style.display = 'block';
         registerForm.style.display = 'none';
-        // Optional: you might want to adjust 'required' attributes here
     } else {
         loginForm.style.display = 'none';
         registerForm.style.display = 'block';
-        // Optional: you might want to adjust 'required' attributes here
     }
 }
 
-// Ensure the correct form is shown on initial page load
 document.addEventListener('DOMContentLoaded', (event) => {
     toggleForm();
 });
